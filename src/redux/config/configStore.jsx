7d334bbc05f99dda8b31c76611170199d3daae75
음지훈 <jhmc5268@naver.com>
2023-03-06 01:01:06 +0900

    Feat: 전체강좌리스트Get

--- src/redux/config/configStore.jsx
+++ src/redux/config/configStore.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import modallogin from "../module/login";
 import register from "../module/signup";
 import postLogin from "../module/postLogin";
+import getCurriculmList from "../module/getcurriculum";
 
 const store = configureStore({
   reducer: {
     modal: modallogin.reducer,
     register: register.reducer,
     login: postLogin.reducer,
+    getCurriculmList: getCurriculmList.reducer
   },
 });
 
